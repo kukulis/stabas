f22a389dad065cdb8969dd9e3a5333ce818a99fd
Giedrius Tumelis <gtu@tpm.com>
2025-10-07 16:47:30 +0300

    sukurtos js klasės

--- application/assets/js/messages.js
+++ application/assets/js/messages.js
@@ -1,3 +1,34 @@
 function testMessage(msg) {
-    alert (msg);
+    alert(msg);
+}
+
+class Message {
+    Message(text) {
+        this.text = text;
+        // TODO
+    }
+
+
+}
+
+class MessagesList {
+    MessagesList() {
+        this.messages = [];
+        this.messagesListId = 'messagesList';
+    }
+
+
+    addMessage(message) {
+        this.messages.add(message)
+    }
+
+    /**
+     * @return dom element with the messages containing data
+     */
+    renderMessages() {
+        let messagesList =  document.createElement('div');
+        messagesList.setAttribute('id', this.messagesListId );
+
+    }
+
 }
