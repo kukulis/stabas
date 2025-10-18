a9995bc455d7823d0f45c7edea212031ee44a7b4
giedrius <giedrius@inkodus.lt>
2025-10-18 09:57:05 +0300

    Further moving with prototype.

--- application/assets/js/messages.js
+++ application/assets/js/messages.js
@@ -2,16 +2,21 @@ function testMessage(msg) {
     alert(msg);
 }
 
+// TODO Task
 class Message {
-    constructor(text) {
+    constructor(text, id, date) {
         this.text = text;
+        this.id = id;
+        this.date = date;
     }
 }
 
+// TODO TasksList
 class MessagesList {
-    constructor() {
+    constructor(messageDisplayContainerId) {
         this.messages = [];
         this.messagesListId = 'messagesList';
+        this.messageDisplayContainerId = messageDisplayContainerId;
     }
 
 
@@ -35,7 +40,21 @@ class MessagesList {
             messagesListElement.appendChild(messageElement)
         }
 
+        let addButton = document.createElement('button');
+        addButton.appendChild(document.createTextNode('+'));
+        messagesListElement.appendChild(addButton);
+
+        addButton.addEventListener('click', (e)=>this.addMessagePressed(e));
+
         return messagesListElement;
+    }
+
+    addMessagePressed(event) {
+        alert('TODO add message');
+    }
+
+    renderMessageDetails(message) {
+
     }
 
 }
