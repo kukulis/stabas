0d3073dd30140b43adc7306b4860c0f39b051d2a
giedrius <giedrius@inkodus.lt>
2025-11-05 23:08:27 +0200

    Skeleton for participants page.

--- application/assets/js/participants-component.js
+++ application/assets/js/participants-component.js
@@ -1,1 +1,37 @@
-// TODO
+class ParticipantsComponent {
+    /**
+     * @type {Dispatcher}
+     */
+    dispatcher = null;
+
+    /**
+     * @type {[Participant]}
+     */
+    participants = [];
+
+    /**
+     *
+     * @param dispatcher {Dispatcher}
+     */
+    constructor(dispatcher) {
+        this.dispatcher = dispatcher;
+    }
+
+    loadParticipants() {
+        console.log('TODO loadParticipants')
+    }
+
+    renderParticipants() {
+        let participantsDiv = document.createElement('div');
+        participantsDiv.appendChild(document.createTextNode('TODO participants div'))
+
+        return participantsDiv;
+    }
+
+    static async initialize(dispatcher) {
+        let participantsComponent = new ParticipantsComponent(dispatcher)
+        participantsComponent.loadParticipants();
+
+        return participantsComponent
+    }
+}
