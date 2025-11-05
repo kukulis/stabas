a9934d3c92c0bba4b0c97754850e060bc722a03f
giedrius <giedrius@inkodus.lt>
2025-11-05 21:36:56 +0200

    Making menu to a separate template

--- application/main.go
+++ application/main.go
@@ -4,7 +4,10 @@ import (
 	"darbelis.eu/stabas/api"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"html/template"
+	"io/fs"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -17,18 +20,30 @@ func main() {
 		"templates/settings.html",
 		"templates/test.html",
 	)
+
+	templatesDir := os.DirFS("templates")
+	menuContent, ferr := fs.ReadFile(templatesDir, "menu.html")
+	if ferr != nil {
+		fmt.Println("Error reading menu.html " + ferr.Error())
+	}
+
+	menuContentHtml := template.HTML(menuContent)
+
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
+		c.HTML(http.StatusOK, "index.html", gin.H{"menu": menuContentHtml})
 	})
 	router.GET("/tasks", func(c *gin.Context) {
 		fmt.Println("Tasks reloaded")
-		c.HTML(http.StatusOK, "tasks.html", gin.H{})
+		c.HTML(http.StatusOK, "tasks.html", gin.H{"menu": menuContentHtml})
 	})
 	router.GET("/participants", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "participants.html", gin.H{})
+		c.HTML(http.StatusOK, "participants.html", gin.H{"menu": menuContentHtml})
 	})
 	router.GET("/test", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "test.html", gin.H{})
+		c.HTML(http.StatusOK, "test.html", gin.H{"menu": menuContentHtml})
+	})
+	router.GET("/settings", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "settings.html", gin.H{"menu": menuContentHtml})
 	})
 
 	router.GET("/api/tasks", func(c *gin.Context) { api.TaskControllerInstance.GetAllTasks(c) })
