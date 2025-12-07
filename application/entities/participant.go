b577e19c7ed2c3eca635e2b933b52f440ebce346
giedrius <giedrius@inkodus.lt>
2025-12-07 21:13:53 +0200

    Created token and password to the Participant entity, for a future functionality.

--- application/entities/participant.go
+++ application/entities/participant.go
@@ -1,7 +1,9 @@
 package entities
 
 type Participant struct {
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
-	Deleted bool   `json:"deleted"`
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Deleted  bool   `json:"deleted"`
+	Token    string `json:"token"`
+	Password string `json:"password"`
 }
